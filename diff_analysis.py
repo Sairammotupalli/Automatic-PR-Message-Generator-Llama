fef9e0b6b5420700e4be358f949a6d1583aa6f05
Sairammotupalli <83081096+Sairammotupalli@users.noreply.github.com>
2024-10-30 19:32:35 -0700

    Update diff_analysis.py

--- diff_analysis.py
+++ diff_analysis.py
@@ -4,13 +4,11 @@ from prompt import Prompt
 import uuid
 import inspect
 
-
 class DiffAnalysis:
     DEFAULT_PROMPT = Prompt(
         inspect.cleandoc(
             """ 
             Act like a very senior software developer caring for his coworkers. Summarize the changes introduced by the pull request whose diff is provided below. Each change should be described with a bullet point. Suggest refactoring and modifications to improve the code. 
-
 
             Here is the pull request diff:
             """
@@ -21,9 +19,8 @@ class DiffAnalysis:
         NOT_STARTED = "NOT_STARTED"
         FINISHED = "FINISHED"
 
-    def __init__(self, diff: str, openai_client):
+    def __init__(self, diff: str):
         self.id = f"{hash(diff)}-{uuid.uuid4()}"
-        self.openai_client = openai_client
         self.diff = diff
         self.state = self.State.NOT_STARTED
         self.completion_history = []
@@ -36,19 +33,18 @@ class DiffAnalysis:
         diff_prompt = Prompt(self.diff, max_tokens)
         splitted_diff_prompts = diff_prompt.split()
 
-        print(f"generating response for {len(splitted_diff_prompts)} prompts...")
+        print(f"Generating response for {len(splitted_diff_prompts)} prompts...")
         diff_analysis = ""
         for i, prompt in enumerate(splitted_diff_prompts):
-            completion = Completion(
-                self.DEFAULT_PROMPT.concat(prompt.wrap("###")), self.openai_client
-            )
+            completion = Completion(self.DEFAULT_PROMPT.concat(prompt.wrap("###")))
             completion.complete()
             self.completion_history.append(completion)
             segment_text = completion.result
 
             diff_analysis += segment_text
-            print(f"Generated prompt for segment {i+1} continuing ...")
-        print(f"generated {len(splitted_diff_prompts)}")
+            print(f"Generated prompt for segment {i + 1}, continuing...")
+        
+        print(f"Generated {len(splitted_diff_prompts)} segments.")
         self.result = diff_analysis
         self.state = self.State.FINISHED
 
