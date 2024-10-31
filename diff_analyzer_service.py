bec10ad9466bd65fad30f98a84b19a0dc6cfc347
Sairammotupalli <83081096+Sairammotupalli@users.noreply.github.com>
2024-10-30 19:32:52 -0700

    Update diff_analyzer_service.py

--- diff_analyzer_service.py
+++ diff_analyzer_service.py
@@ -3,20 +3,17 @@ from diff_analysis import DiffAnalysis
 import logging
 import inspect
 
-
 class PromptsAreNotEmpty(Exception):
     pass
 
-
 class DiffAnalyzerService:
-    def __init__(self, diff_file: str, openai_client):
+    def __init__(self, diff_file: str):
         self.diff_file = diff_file
-        self.openai_client = openai_client
 
     def analyse_diff(self):
         with open(self.diff_file, "r") as f:
             diff_text = f.read()
-            diff_analysis = DiffAnalysis(diff_text, self.openai_client)
+            diff_analysis = DiffAnalysis(diff_text)
             diff_analysis.exec()
             logging.info(f"analysis-{diff_analysis.id} - Analysis finished")
             logging.info(f"analysis-{diff_analysis.id} - Analysed {diff_text}")
